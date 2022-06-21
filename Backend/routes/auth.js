a132f72d5812598ca768b85a2bf96f765a6d0b54
Usama <usamamughal4949@gmail.com>
2022-06-22 02:26:33 +0500

    Hashed password with salt and paper and made web tokens

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,9 @@ const express=require('express')
 const User = require('../models/User')
 const router=express.Router()
 const { body, validationResult } = require('express-validator');
+const bcrypt=require('bcryptjs')
+var jwt=require('jsonwebtoken')
+const JWT_SECRET='usamaisagoodb$oy'
 //CREATE A USER USING : POST "/api/auth/createuser".No login required
 router.post('/createuser',[
     body('email','Enter a valid mail').isEmail(),
@@ -20,16 +23,25 @@ router.post('/createuser',[
     if(user){
         return res.status(400).json({ error:"Sorry a user with the same email already exists!" });
     }
+    const salt=await bcrypt.genSalt(10)
+    const secPass= await bcrypt.hash(req.body.password,salt)
      user = await User.create({
         name: req.body.name,
-        password: req.body.password,
+        password:secPass,
         email: req.body.email
       })
-      res.json({message:"User Created!"})
+      const data={
+        user:{
+            id:user.id
+        }
+      }
+      const authToken=jwt.sign(data,JWT_SECRET)
+      res.json({authToken})
+    //   res.json({message:"User Created!"})
     }
     catch
     {
-        console.error(error.message)
+       // console.error(error.message)
         res.status(500).send("Some error occured!")
     }
 })
