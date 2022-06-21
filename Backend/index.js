0f88f5a4056202b6a8d363dc574f2d92491b4819
Usama <usamamughal4949@gmail.com>
2022-06-21 14:16:35 +0500

    created routes and schema for myNotebook

--- Backend/index.js
+++ Backend/index.js
@@ -1,12 +1,15 @@
 const connectToMongo = require('./db')
-const express = require('express')
+const express = require('express');
+const { request } = require('express');
 const app = express()
 const port = 3000
 connectToMongo();
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
   
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port http://localhost:${port}`)
   })
---
+++ Backend/models/User.js
@@ -0,0 +1,21 @@
+//Model filename should start with capital letter
+const mongoose=require('mongoose');
+const userSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:
+    {
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:
+    {
+        type:String,
+        required:true,
+    },
+    date: { type: Date, default: Date.now }
+  });
+  module.exports=mongoose.model("user",userSchema)
