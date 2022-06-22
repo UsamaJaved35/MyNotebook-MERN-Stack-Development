f93081513c787eba05b64b3af9751f7561d013e8
Usama <usamamughal4949@gmail.com>
2022-06-23 01:03:16 +0500

    Created a middleware to decode user from a JWT

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,6 +4,7 @@ const router=express.Router()
 const { body, validationResult } = require('express-validator');
 const bcrypt=require('bcryptjs')
 var jwt=require('jsonwebtoken')
+var fetchuser=require('../middlewares/fetchuser')
 const JWT_SECRET='usamaisagoodb$oy'
 //CREATE A USER USING : POST "/api/auth/createuser".No login required
 router.post('/createuser',[
@@ -23,8 +24,10 @@ router.post('/createuser',[
     if(user){
         return res.status(400).json({ error:"Sorry a user with the same email already exists!" });
     }
+    //adding salt to password and hashing it
     const salt=await bcrypt.genSalt(10)
     const secPass= await bcrypt.hash(req.body.password,salt)
+    //creating user
      user = await User.create({
         name: req.body.name,
         password:secPass,
@@ -45,12 +48,15 @@ router.post('/createuser',[
         res.status(500).send("Some error occured!")
     }
 })
+
+
+//LOGIN A USER USING : POST "/api/auth/login". login required
 router.post('/login',[
     body('email','Enter a valid mail').isEmail(),
-    // password must be at least 5 chars long
+    // password must exist in database
     body('password',"Password can't be blank").exists()
 ], async(req,res)=>{
-   // try{
+    try{
     ////If  there are errors return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -72,13 +78,27 @@ router.post('/login',[
             id:user.id
         }
       }
+      //creating an auth token to use in  any endpoint where user needs to login
       const authToken=jwt.sign(data,JWT_SECRET)
       res.json({authToken})
-   // }
-    // catch(error)
-    // {
-    //     console.error(error.message)
-    //     res.status(500).send("Some error occured!")
-    // }
+   }
+    catch(error)
+    {
+        console.error(error.message)
+        res.status(500).send("Some error occured!")
+    }
+})
+
+//GET A USER USING : POST "/api/auth/getuser". login required
+router.post('/getuser',fetchuser, async(req,res)=>{
+    try {
+    const userId=req.user.id
+    const user=await User.findById(userId).select("-password")
+    res.send({user})        
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Some error occured!")
+    }
+
 })
 module.exports=router
